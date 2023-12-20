92a34524a98946d58628992704447b72e025211a
nandabi <valerie.nandabi@student.moringaschool.com>
2023-12-21 00:32:41 +0300

    commit message

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Code your solutions in this file
+function writeCards([Guadalupe, Ollie,Aki], birthday) {
+    return (writeCards = [
+      `Thank you, ${Guadalupe}, for the wonderful ${birthday} gift!`,
+      `Thank you, ${Ollie}, for the wonderful ${birthday} gift!`,
+      `Thank you, ${Aki}, for the wonderful ${birthday} gift!`,
+    ]);
+debugger;
+  }
+const num = 10;
+function countDown(num) {
+    while (num >= 0) {
+      console.log(num);
+      num--;
+    }
+    return num;
+  
+}
